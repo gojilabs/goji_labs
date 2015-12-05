2ab33ba1615cde055981b6b26636b7cdf5b46ac5
Adam Sumner <adamsumner@gmail.com>
2015-12-04 17:07:01 -0800

    Setup app config for Sidekiq

--- lib/goji_labs/railtie.rb
+++ lib/goji_labs/railtie.rb
@@ -9,6 +9,13 @@ module GojiLabs
 
     initializer 'goji_labs.load_project' do |app|
       ENV['PROJECT_NAME'] ||= app.class.parent.name.underscore
+
+      if defined?(Sidekiq)
+        app.config.active_job.queue_adapter = :sidekiq
+        app.config.active_job.queue_name_prefix = "#{GojiLabs.project_name}.#{GojiLabs.env}"
+        app.config.active_job.queue_name_delimiter = '.'
+      end
+
       GojiLabs.load_project
     end
   end
